6b1b2c2cc29c82b65b251419a8c687de68673559
huangrong <1011486768@qq.com>
2019-07-21 20:39:58 +0800

    '多种保存方法'

--- qsbk/qsbk/pipelines.py
+++ qsbk/qsbk/pipelines.py
@@ -27,22 +27,42 @@
 #         return item
 
 
-from scrapy.exporters import JsonItemExporter
+# from scrapy.exporters import JsonItemExporter
+#
+#
+# class QsbkPipeline(object):
+#
+#     def __init__(self):
+#         self.f = open("data.json", "wb")
+#         self.exporter = JsonItemExporter(self.f, ensure_ascii=False, encoding="utf-8")
+#         self.exporter.start_exporting()
+#
+#     def open_spider(self, spider):
+#         print("爬虫已爬取到数据，开始保存")
+#
+#     def close_spider(self, spider):
+#         print("数据保存成功")
+#         self.exporter.finish_exporting()
+#         self.f.close()
+#
+#     def process_item(self, item, spider):
+#         self.exporter.export_item(item)
+#         return item
+
+from scrapy.exporters import JsonItemExporter,JsonLinesItemExporter
 
 
 class QsbkPipeline(object):
 
     def __init__(self):
         self.f = open("data.json", "wb")
-        self.exporter = JsonItemExporter(self.f, ensure_ascii=False, encoding="utf-8")
-        self.exporter.start_exporting()
+        self.exporter = JsonLinesItemExporter(self.f, ensure_ascii=False, encoding="utf-8")
 
     def open_spider(self, spider):
         print("爬虫已爬取到数据，开始保存")
 
     def close_spider(self, spider):
         print("数据保存成功")
-        self.exporter.finish_exporting()
         self.f.close()
 
     def process_item(self, item, spider):
