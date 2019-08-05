066fadbd99449c49d5102272526011deb4c7ac6b
huangrong <1011486768@qq.com>
2019-08-05 13:09:46 +0800

    '1'

--- fangtianxia_spider/fangtianxia_spider/items.py
+++ fangtianxia_spider/fangtianxia_spider/items.py
@@ -8,7 +8,35 @@
 import scrapy
 
 
-class FangtianxiaSpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class NewHouseItem(scrapy.Item):
+    """
+    新房
+    """
+    name = scrapy.Field()
+    location = scrapy.Field()
+    price = scrapy.Field()
+    house_type = scrapy.Field()
+    for_sale = scrapy.Field()
+    phone_num = scrapy.Field()
+    size = scrapy.Field()
+    detail_url = scrapy.Field()
+    tag = scrapy.Field()
+
+
+class StockHouseItem(scrapy.Item):
+    """
+    二手房
+    """
+    name = scrapy.Field()
+    house_type = scrapy.Field()
+    size = scrapy.Field()
+    floor = scrapy.Field()
+    orientation = scrapy.Field()
+    build_year = scrapy.Field()
+    owner = scrapy.Field()
+    phone_num = scrapy.Field()
+    location = scrapy.Field()
+    detail_url = scrapy.Field()
+    totle_price = scrapy.Field()
+    avg_price = scrapy.Field()
+    tag = scrapy.Field()
--- fangtianxia_spider/fangtianxia_spider/spiders/ftx_spider.py
+++ fangtianxia_spider/fangtianxia_spider/spiders/ftx_spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class FtxSpiderSpider(scrapy.Spider):
@@ -8,10 +9,47 @@ class FtxSpiderSpider(scrapy.Spider):
     start_urls = ["https://www.fang.com/SoufunFamily.htm"]
 
     def parse(self, response):
-        trs = response.xpath("//table[@class='table01']/tr")
+        trs = response.xpath("//*[@id='senfe']//tr")
+        province = None
         for tr in trs:
-            province = tr.xpath(".//td[not(@class)]")[1]
-            city = tr.xpath(".//td[not(@class)]")[2]
-            print(province)
-            print(city)
+            province_item = tr.xpath(".//td[not(@class)]")[0].xpath(".//text()").get()
+            province_item = re.sub(r"\s", "", province_item)
+            if province_item:
+                province = province_item
+            # 跳过海外
+            if province == "其它":
+                continue
+            cities_info = tr.xpath(".//td[not(@class)]")[1].xpath(".//a")
+            for city_info in cities_info:
+                city_url = city_info.xpath(".//@href").get()
+                city_name = city_info.xpath(".//text()").get()
+                city_uri = city_url.split("//")[1].split(".")[0]
+                if city_uri == "bj":
+                    new_house_url = "https://newhouse.fang.com/house/s/"
+                    stock_house_url = "https://esf.fang.com/"
+                else:
+                    new_house_url = "https://{}.newhouse.fang.com/house/s/".format(city_uri)
+                    stock_house_url = "https://{}.esf.fang.com/".format(city_uri)
+                yield scrapy.Request(url=new_house_url, callback=self.parse_new_house,
+                                     meta={"info": (province, city_name)})
+                yield scrapy.Request(url=stock_house_url, callback=self.parse_stock_house,
+                                     meta={"info": (province, city_name)})
+                break
             break
+
+    def parse_new_house(self, response):
+        province, city_name = response.meta.get("info")
+        print(province, city_name)
+        lis = response.xpath('//*[@id="newhouse_loupai_list"]/ul/li')
+        for li in lis:
+            name_info = li.xpath('.//div[contains(@class,"house_value")]/div[@class="nlcd_name"]/a')
+            if not name_info:
+                continue
+            name = name_info.xpath('.//text()').get().strip()
+            detail_url = response.urljoin(name_info.xpath('.//@href').get())
+            house_type = li.xpath('.//div[contains(@class,"house_type")]/a/text()').getall()
+            house_type = list(map(lambda x: re.sub(r"\s", "", x), house_type))
+            print(name, detail_url, house_type)
+
+    def parse_stock_house(self, response):
+        pass
